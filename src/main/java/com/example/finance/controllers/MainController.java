c5528a19ff026222d37a0eba6a93da5c91aba065
max <max.code.mail@gmail.com>
2021-04-25 01:43:39 +0300

    * cors

--- src/main/java/com/example/finance/controllers/MainController.java
+++ src/main/java/com/example/finance/controllers/MainController.java
@@ -4,12 +4,14 @@ import com.example.finance.models.responses.CurrencyResponse;
 import com.example.finance.models.responses.DataResponse;
 import com.example.finance.services.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(path = "/")
 public class MainController {
